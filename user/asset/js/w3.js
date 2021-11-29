418103edb5f9bcf223b30810b344bc9ad8a79e5a
slowjam83 <pipio8310@gmail.com>
2021-11-29 17:58:10 +0900

    no message

--- user/asset/js/w3.js
+++ user/asset/js/w3.js
@@ -238,8 +238,9 @@ w3.includeHTML = function(cb) {
     if (file) {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          elmnt.innerHTML = this.responseText;
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           elmnt.removeAttribute("w3-include-html");
           w3.includeHTML(cb);
         }
