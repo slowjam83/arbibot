3e8c6c0f36695da5dbaf1f3f9ce39a6d6c2b78dc
slowjam83 <pipio8310@gmail.com>
2021-11-30 09:56:16 +0900

    no message

--- user/asset/js/common.js
+++ user/asset/js/common.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     function btnTop() {
         var scrollTop = $(window).scrollTop();
         
-        if (scrollTop > 980) {
+        if (scrollTop > 320) {
             $("#goTop").stop().show().animate({
                 "opacity": "1"
             }, 300);
